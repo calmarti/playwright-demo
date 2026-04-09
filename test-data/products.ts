5de2ed73202bfc9047c7d0dc790df53a7571d0f0
Ivan Martinez <ivan.martinez@atmira.com>
2026-04-09 13:51:39 +0200

    add products.ts (test data)

---
+++ test-data/products.ts
@@ -0,0 +1,9 @@
+import { Item } from "../types";
+
+
+export const testItem: Item = {
+    id: 2,
+    name: 'Sauce Labs Onesie',
+    description: 'Rib snap infant onesie for the junior automation engineer in development. Reinforced 3-snap bottom closure, two-needle hemmed sleeved and bottom won\'t unravel.',
+    price: 7.99
+}
