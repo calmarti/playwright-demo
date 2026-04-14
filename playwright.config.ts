8c2a78fae3955fbd0c1c4fd8306b71e3f316bff2
Ivan Martinez <ivan.martinez@atmira.com>
2026-04-14 09:21:10 +0200

    add junit-report folder to reports config and .yml, update .gitignore

--- playwright.config.ts
+++ playwright.config.ts
@@ -28,7 +28,7 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
     ['html', { open: 'never' }],
-    ['junit', { outputFile: 'results.xml'} ]],
+    ['junit', { outputFile: 'junit-report/results.xml'} ]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
